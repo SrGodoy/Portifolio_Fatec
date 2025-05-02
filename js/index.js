175a74360513074242cacad26d8dbc812d98ecf9
SrGodoy <luccasgodoy2012@gmail.com>
2025-05-02 12:53:29 -0300

    modificando css e adicionando js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-exemplo = 1;
+
+// Mostra o botão ao rolar a pagina
+window.addEventListener('scroll', function() {
+    const botaoInicio = document.querySelector('.botao_inicio');
+    if (window.scrollY > 300) {
+        botaoInicio.classList.add('mostrar_botao');
+    } else {
+        botaoInicio.classList.remove('mostrar_botao');
+    }
+});
+
+
+
+
+//Rolagem suave
+document.querySelector('botao_inicio').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
